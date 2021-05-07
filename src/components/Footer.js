0fdd3be70cdb11a503a5eeafccd01e43a5b4ad97
Jassica <jassica0337.be20@chitkara.edu.in>
2021-05-07 22:57:09 +0530

    addfooter+mediaquer(nav)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,71 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <div style={{paddingTop:'4em'}}>
-            <p>A real footer!!!</p>
-            <p>Helos</p>
+        <div >
+             <div class="icons">
+    <i class="fa fa-facebook-f"></i>
+    <i class="fa fa-instagram"></i>
+    <i class="fa fa-pinterest-p"></i>
+    <i class="fa fa-twitter"></i>
+    <i class="fa fa-youtube"></i>
+  </div>
+
+  <div class="row">
+    <div class="column sidel">
+      <h2 class="foot set1">Our services</h2>
+      <a href="">Delivery</a>
+      <a href="">Grocery Pickup</a>
+      <a href="">Money Center</a>
+      <a href="">Our Credit Card</a>
+      <a href="">Weekly AD</a>
+      <a href="">Other Services</a>
+    </div>
+
+    <div class="column middle">
+      <h2 class="foot set3">Customer services</h2>
+
+      <a href="">Return</a>
+      <a href="">Help Center</a>
+      <a href="">Accessibility</a>
+      <a href="">Contact Us</a>
+      <a href="">Store PickUp</a>
+      <a href="">Product Recalls</a>
+    </div>
+
+    <div class="column side">
+      <h2 class="foot set2">Get to know us</h2>
+
+      <a href="">Careers</a>
+      <a href="">Our Suppliers</a>
+      <a href="">Digital Museum</a>
+      <a href="">Our company</a>
+      <a href="">Store Directory</a>
+      <a href="">Advertise With Us</a>
+    </div>
+
+
+    <div class="column sider">
+      <h2 class="foot set4">In The Spotlight</h2>
+      <a href="">Skateboard</a>
+      <a href="">Outdoor Furniture</a>
+      <a href="">Trampoline</a>
+      <a href="">PS4</a>
+      <a href="">Nintendo Switch Lite</a>
+      <a href="">Portable Air Conditioners</a>
+    </div>
+
+
+  </div>
+  <div className="HRline"></div>
+  <div class="row2">
+    <div class="footer right">
+      © 2021 Walmart. All Rights Reserved.
+    </div>
+    <div class="footer left">
+      To ensure we are able to help you as best we can, please include your reference number: SEOFEAPHIE
+    </div>
+  </div>
+            
         </div>
     )
 }
