d9a41a43b4c28ba9fa10b5a189fa655b90e88710
itsjj <jinendra0348.be20@chitkara.edu.in>
2021-05-07 18:53:06 +0530

    Nav Added

---
+++ src/components/menuData.js
@@ -0,0 +1,49 @@
+import cartBlack from "../images/nav/cartblack.svg"
+import locationblack from "../images/nav/locationblack.svg"
+import phoneblack from "../images/nav/phoneblack.svg"
+// import listblack from "../images/listblack.svg"
+
+export const menuData =[
+    {
+        id:0,
+        label:"Home",
+        url:"https://xyz.com",
+        img:cartBlack
+
+    },
+    {
+        id:1,
+        label:"Location",
+        url:"https://xyz.com",
+        img:locationblack
+
+    },
+    {
+        id:2,
+        label:"Contact Us",
+        url:"https://xyz.com",
+        img:phoneblack
+
+    },
+    {
+        id:3,
+        label:"My Items",
+        url:"https://xyz.com",
+        img:cartBlack
+
+    },
+    {
+        id:4,
+        label:"Privacy Policy",
+        url:"https://xyz.com",
+        img:cartBlack
+
+    },
+    {
+        id:5,
+        label:"Today Deals",
+        url:"https://xyz.com",
+        img:cartBlack
+    },
+];
+export default menuData;
