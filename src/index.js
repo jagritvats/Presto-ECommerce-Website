5dfbbab43b458ea08f874813d58e9839ee84fba6
Jagrit <69034224+jagritvats@users.noreply.github.com>
2021-05-11 15:41:11 +0530

    Removed Compose Error

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import {createStore, applyMiddleware,compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
 import allReducer from './reducers';
 import {Provider} from 'react-redux';
 
 const store = createStore(
-    allReducer, compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    allReducer, applyMiddleware(thunk)
 );
 
 // on cart change the sessionStorage is updated
