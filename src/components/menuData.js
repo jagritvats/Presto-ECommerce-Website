c3caad13c17b57739d274d74e95cbe894ed2bebd
Jagrit <69034224+jagritvats@users.noreply.github.com>
2021-05-11 17:28:33 +0530

    UI Changes and mobile menu

--- src/components/menuData.js
+++ src/components/menuData.js
@@ -1,6 +1,6 @@
 import cartBlack from "../images/nav/cartblack.svg"
 import homeIcon from "../images/others/home.svg"
-
+import orderIcon from "../images/nav/orders.svg"
 export const menuData =[
     {
         id:0,
@@ -14,7 +14,11 @@ export const menuData =[
         label:"My Cart",
         url:"/cart",
         img:cartBlack
-
+    },{
+        id:2,
+        label:"My Orders",
+        url:"/orders",
+        img:orderIcon
     }
 ];
 export default menuData;
